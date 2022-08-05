66ff771d5a69ed49b1de4a0fd5d1bd069ff174ca
Isaac <isaacolaolu10@gmail.com>
2022-08-05 12:06:46 +0100

    Major page fixes

--- src/containers/ShopFeed/ShopFeed.js
+++ src/containers/ShopFeed/ShopFeed.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import Svg from "src/components/Svg/Svg";
 import ProductModal from "src/components/Modals/ProductModal/ProductModal";
 import styles from "src/containers/ShopFeed/ShopFeed.module.scss";
@@ -22,7 +22,11 @@ function ShopFeed({ products }) {
   const [selectedProduct, setSelectedProduct] = useState({});
   const [viewProduct, setViewProduct] = useState();
 
-  // console.log("===products===", products);
+  const sortProducts = [...products]
+
+  sortProducts.sort((a, b) => {
+    return b.count - a.count;
+  })
 
   const handleAddToCard = async (product) => {
     const lineItem = {
@@ -79,14 +83,14 @@ function ShopFeed({ products }) {
         </div>
         <div className={styles.grid}>
           {products.length > 0 &&
-            products.map((product, i) => {
-              const img = product.image;
+            sortProducts.map((product, i) => {
+              const img = product?.product.image;
               return (
                 <div key={i} className={styles.grid__item}>
                   <figure>
                     <Image loader={() => img} objectFit="cover" alt={product.name} src={img} layout="fill" />
                     <div className={styles.grid__item_options}>
-                      {!product.variation && (
+                      {!product?.product?.variation && (
                         <button onClick={() => handleAddToCard(product)}>
                           <span>Add to Basket</span>
                           <Svg symbol="shopping-basket" />
@@ -109,17 +113,17 @@ function ShopFeed({ products }) {
                       </button>
                     </div>
                   </figure>
-                  <h3>{product.name}</h3>
-                  {product.variation ? (
+                  <h3>{product?.product?.name}</h3>
+                  {product?.product?.variation ? (
                     <p>
-                      {formatToCurrency(MinAmount(product.variation))} - {formatToCurrency(MaxAmount(product.variation))}
+                      {formatToCurrency(MinAmount(product?.product?.variation))} - {formatToCurrency(MaxAmount(product?.product?.variation))}
                     </p>
                   ) : (
-                    <p>{formatToCurrency(product.price)}</p>
+                    <p>{formatToCurrency(product?.product?.price)}</p>
                   )}
                 </div>
               );
-            })}
+            }).sort((a, b) => b.count - a.count)}
         </div>
       </div>
     </>
